b12920626c90eea2da913bf43d6352ce3e4d6cc2
DESTRUKT\augus <augustodafonte@gmail.com>
2023-03-18 12:54:51 +0000

    seeder and middleware

---
+++ Models/Product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a Product title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
+  price: {
+    type: String,
+    required: [true, 'Please add price']
+  },
+  products: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'products',
+    required: true
+  }
+});
---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const product = require('./models/product');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const products = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/products.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Product.create(Products);
+
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Product.deleteMany();
+
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
